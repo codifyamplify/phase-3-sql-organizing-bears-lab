23d6d6f46999bf7ef07998b4283253bef3674ef8
David Whitney <codifyamplify@gmail.com>
2023-01-17 17:24:07 -0500

    commit tree 8e9b7cea466313e7660e249da3eb88726644b972

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "M", "black", "calm", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "brown", "grumpy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 7, "M", "black", "stern", 0);
+INSERT INTO bears VALUES (5, "Melissa", 3, "F", "brown", "happy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 10, "M", "green", "grumpy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 6, "F", "brown", "kind", 1);
+INSERT INTO bears VALUES (8, null, 1, "M", "brown", "sleepy", 1);
